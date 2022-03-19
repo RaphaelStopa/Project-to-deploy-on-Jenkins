7402190f3269891dc786a7fa234226a5057d7fcf
RaphaelEsparta <83084343+RaphaelEsparta@users.noreply.github.com>
2022-03-18 22:57:01 -0300

    no message

---
+++ pipeline/JenkinsTestPipeline.java
@@ -0,0 +1,5 @@
+public class JenkinsTestPipeline {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+    }
+}
---
+++ src/main/java/com/mycompany/myapp/web/rest/JenkinsTestPipeline.java
@@ -0,0 +1,7 @@
+package com.mycompany.myapp.web.rest;
+
+public class JenkinsTestPipeline {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+    }
+}
